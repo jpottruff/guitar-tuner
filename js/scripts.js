889bc33a14230b13b223a33818e68251dd8542cb
Jeff Pottruff <jeffpottruff@gmail.com>
2020-05-02 16:38:06 -0400

    restrcture files, couple small tweaks

---
+++ js/scripts.js
@@ -0,0 +1,42 @@
+// After page has loaded
+document.addEventListener("DOMContentLoaded", function() {
+    // 1. Get all the notes from the document
+    const notes = document.querySelectorAll('.note');
+   
+    // 2. Add event Listeners
+    window.addEventListener('keydown', handleKeydown);
+    notes.forEach(note => note.addEventListener("click", handleClick));
+    notes.forEach(note => note.addEventListener('transitionend', removeTransition));
+});
+
+
+// Functions
+
+function handleClick(e) {
+    playSound(e.target.getAttribute("data-key"));
+}
+
+function handleKeydown(e) {
+    playSound(e.keyCode);
+}
+
+function playSound(keyCode) {
+    // 1. Define the audio that needs to be played based off the key code
+    const audio = document.querySelector(`audio[data-key="${keyCode}"]`);
+    
+    // If there's no audio to play return
+    if (!audio) return;
+
+    // 2. Selecting the Note that was pressed so it can be animated
+    const note = document.querySelector(`.note[data-key="${keyCode}"]`);
+
+    // 3. Play the audio and add a class of 'playing'
+    audio.currentTime = 0;  // allows the button to be pressed and trigger again without waiting for sound to finish
+    audio.play();
+    note.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
